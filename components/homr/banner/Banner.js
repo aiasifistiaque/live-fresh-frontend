e23d8e295ba44da81b52943364833540e0e0543e
Asif Istiaque <asifistiaque.ai@gmail.com>
2022-07-16 04:56:55 +0600

    fixes

--- components/homr/banner/Banner.js
+++ components/homr/banner/Banner.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import HomeSection from '../section/HomeSection';
 import styles from './Styles.module.css';
+import bg from '../../../public/test/hero.jpeg';
 
 const Banner = () => {
 	const style = { height: '90vh', marginTop: '4vh' };
 	return (
-		<div className={styles.container}>
+		<div
+			className={styles.container}
+			style={{
+				backgroundImage: `url(${bg.src})`,
+				width: '100%',
+				height: '100%',
+			}}>
 			<div className={styles.main}>
 				<div className={styles.left}>
 					<img src={`/test/logo-white.png`} alt='LIVE FRESH' />
